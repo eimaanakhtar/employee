97354feb7242578138b5998eba7cbcc8ff2cf0d9
Eimaan Akhtar <eimaanakhtar12@gmail.com>
2025-09-19 15:00:21 -0400

    finished question 11. I redid the question 13-14 fixing some things and taking out the errors.

--- Employee.java
+++ Employee.java
@@ -9,8 +9,7 @@ public class Employee{
     private int yearOfBirth;
     private boolean onLeave;
     private double hourlyWage;
-    private double unpaidHours; setter = 0;
-                                getter = calculatePay();
+    private double unpaidHours = 0; // unpaidHours starts at 0
 
     public Employee(String fullname, int yearOfBirth, double hourlyWage){
         this.fullname = fullname;
@@ -20,6 +19,13 @@ public class Employee{
         unpaidHours = 0.0;
     }
     
+    public double getUnpaidHours() {
+    return unpaidHours;
+}
+
+public void setUnpaidHours(double unpaidHours) {
+    this.unpaidHours = unpaidHours;
+}
     /**
      * Returns true is an employee is above 16 years old
      */
@@ -27,40 +33,36 @@ public class Employee{
         return (currentYear - yearOfBirth);
     }
 
-    /**
-     * Returns true is an employee is above 16 years old
-     */
+   /**
+   * Returns true is an employee is above 16 years old
+   */
     public boolean canDrive(){
         int age = calculateAge(2025);
         if (age<16) {// to be completed
-            System.out.println(Employee + "can drive.");
-    } else {System.out.println(Employee + "must be above 16 years old, before driving.");
-    }
-}
+            System.out.println(fullname + "can drive.");
+            return true;
+    } else {
+        System.out.println(fullname + "must be above 16 years old, before driving.");
+    return false;
 
-public void canDrive() {
-    return Employee + "age:" + age;
+    }
 }
 
     /*
      * Returns the net pay for the outstanding unpaid hours
      */
-    private double calculatePay(){
-        tax = 30;
-        hourlyWage = 10;
-        paySalary = 100;
-        paySalary - tax = totalPaycheck; // to be completed
+    public double calculatePay(){
+        double totalPaycheck = hourlyWage * unpaidHours;
         System.out.println("Total salary is" + totalPaycheck);
+        return totalPaycheck;
     }
 
     /*
      * Output the payment record and resets unpaid hours
      */
     public void paySalary(){
-        this.fullname = "John Deere";
-        this.hourlywage = 0;
-        calculatePay = 70;
-        System.out.println(fullname + "has received a wire transer of" + calculatePay);
+        double pay = calculatePay();
+        System.out.println(fullname + "has received a wire transer of" + pay);
         
         unpaidHours = 0;// to be completed
     }
